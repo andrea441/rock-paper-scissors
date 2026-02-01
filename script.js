c4883575938a9425bfd20930d02ff726e4361ce9
Andrea Burciaga <174824899+andrea441@users.noreply.github.com>
2026-02-01 16:18:26 -0600

    Implement logic for "getComputerChoice" and "getHumanChoice" functions

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello World!')
+function getComputerChoice() {
+    // Get a random integer from 1 to 3, save it in a variable
+    const minCeiled = Math.ceil(1);
+    const maxFloored = Math.floor(3);
+    const random = Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+    // Map that integer to a choise and return it like a string
+    return (random == 1) ? 'rock' : (random == 2) ? 'paper' : 'scissors';
+}
+
+function getHumanChoice() {
+    return prompt('Enter your selected choise (rock, paper or scissor): ');
+}
